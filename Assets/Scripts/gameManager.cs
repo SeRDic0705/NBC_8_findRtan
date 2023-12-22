c06cbb1236ae4be704b4a7952a6deab3fc3a6765
EcchiClone <aa01092308481@gmail.com>
2023-12-22 20:12:07 +0900

    난이도 따른 카드 수 및 배치 2트
    gameManager.cs

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,8 @@ public class gameManager : MonoBehaviour
     public AudioClip wrong;
     public AudioClip shuffle;
 
+    public GameObject NowDifficulty;
+
 
     void Awake()
     {
@@ -32,12 +35,113 @@ public class gameManager : MonoBehaviour
         Time.timeScale = 1.0f;
         audioSource.PlayOneShot(shuffle);
 
+        // 난이도에 따른 이미지 갯수 조정
+        int[] images;
+        NowDifficulty = GameObject.Find("NowDifficulty");
+        string difficulty = NowDifficulty.GetComponent<nowDifficulty>().difficulty;
 
-
+        if (difficulty == "Easy")
         {
+            images = new int[10]; // Easy 난이도는 10개 이미지
+            for (int i = 0; i < images.Length / 2; i++)
+            {
+                images[2 * i] = i;
+                images[2 * i + 1] = i;
+            }
+        }
+        else if (difficulty == "Normal")
+        {
+            images = new int[20]; // Normal 난이도는 20개 이미지
+            for (int i = 0; i < images.Length / 2; i++)
+            {
+                images[2 * i] = i;
+                images[2 * i + 1] = i;
+            }
+        }
+        else // Hard 또는 기타
+        {
+            images = new int[30]; // Hard 난이도는 30개 이미지
+            for (int i = 0; i < images.Length / 2; i++)
+            {
+                images[2 * i] = i;
+                images[2 * i + 1] = i;
+            }
+        }
 
+        // 카드를 섞는 원래 코드
+        //images = images.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
+
+        // 카드를 섞는 새로운 코드
+        for (int i = 0; i < images.Length; i++)
+        {
+            int randomIndex = UnityEngine.Random.Range(0, images.Length);
+            int temp = images[i];
+            images[i] = images[randomIndex];
+            images[randomIndex] = temp;
+        }
+
+
+        // 난이도별로 다른 카드 배치
+        if (difficulty == "Easy")
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                GameObject newCard = Instantiate(card);
+                newCard.transform.parent = GameObject.Find("cards").transform;
+
+                float x;
+                float y;
+
+                x = (i % 3) * 1.3f - 1.3f;
+                y = (i / 3) * 1.3f - 2.3f;
+                if (i >= 3 && i <= 6)
+                {
+                    x = ((i + 1) % 4) * 1.3f - 1.3f - 0.65f;
+                    y = ((i + 1) / 4) * 1.3f - 2.3f;
+                }
+                else if (i > 6)
+                {
+                    x = ((i - 1) % 3) * 1.3f - 1.3f;
+                    y = ((i - 1) / 3) * 1.3f - 2.3f;
+                }
+                newCard.transform.position = new Vector3(x, y, 0);
+
+                string imageName = "image" + images[i].ToString();
+                newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(imageName);
+            }
+        }
+        else if (difficulty == "Normal")
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                GameObject newCard = Instantiate(card);
+                newCard.transform.parent = GameObject.Find("cards").transform;
+
+                float x = (i / 5) * 1.3f - 2.6f + 0.65f;
+                float y = (i % 5) * 1.3f - 3.6f;
+                newCard.transform.position = new Vector3(x, y, 0);
+
+                string imageName = "image" + images[i].ToString();
+                newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(imageName);
+            }
 
         }
+        else
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                GameObject newCard = Instantiate(card);
+                newCard.transform.parent = GameObject.Find("cards").transform;
+
+                float x = (i / 6) * 1.3f - 2.6f;
+                float y = (i % 6) * 1.3f - 3.6f;
+                newCard.transform.position = new Vector3(x, y, 0);
+
+                string imageName = "image" + images[i].ToString();
+                newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(imageName);
+            }
+        }
+
     }
 
     // Update is called once per frame
