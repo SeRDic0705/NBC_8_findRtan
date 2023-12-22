9a7965ae6142d22e9e1b5a1fc699c3a54973d86b
SeRDic0705 <serdic0705@gmail.com>
2023-12-22 15:42:13 +0900

    fix:rename images

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -27,7 +27,7 @@ public class gameManager : MonoBehaviour
     {
         Time.timeScale = 1.0f;
 
-        int[] images = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15 };
+        int[] images = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14 };
 
         images = images.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
 
