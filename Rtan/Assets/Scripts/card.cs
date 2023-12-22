793afea0207b04cc9d01b94e55e3ca0191c6a8e4
SeRDic0705 <serdic0705@gmail.com>
2023-12-22 14:54:14 +0900

    Initial commit

---
+++ Rtan/Assets/Scripts/card.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class card : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void openCard()
+    {
+        Debug.Log("opencard");
+        anim.SetBool("isOpen", true);
+        transform.Find("front").gameObject.SetActive(true);
+        transform.Find("back").gameObject.SetActive(false);
+    }
+}
