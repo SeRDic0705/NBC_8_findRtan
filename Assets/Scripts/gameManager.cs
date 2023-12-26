164369892cb0be3ea0b107d141b4c39f5ad6c476
EcchiClone <aa01092308481@gmail.com>
2023-12-26 11:44:05 +0900

    스프라이트 매핑 코드 롤백 / 주석영문화
    - 스프라이트 이름 롤백(OOO1 → image1)에 따라, 매핑 코드도 롤백
    - 주석영문화

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -23,16 +23,6 @@ public class gameManager : MonoBehaviour
 
     public GameObject NowDifficulty;
 
-    // 이미지 리소스 이름 수정에 따라 nameMap 작성
-    private Dictionary<int, string> nameMap = new Dictionary<int, string>
-    {
-        { 0, "김소이" },
-        { 1, "이승배" },
-        { 2, "이도현" },
-        { 3, "김성우" },
-        { 4, "김준하" }
-    };
-
     void Awake()
     {
         I = this;
@@ -44,51 +34,48 @@ public class gameManager : MonoBehaviour
         Time.timeScale = 1.0f;
         audioSource.PlayOneShot(shuffle);
 
-        // 난이도에 따른 이미지 갯수 조정
+        // Adjusting the Number of Cards According to Difficulty
         int[] images;
         NowDifficulty = GameObject.Find("NowDifficulty");
         string difficulty = NowDifficulty.GetComponent<nowDifficulty>().difficulty;
 
-
-
+        // Easy : 10 Cards(5 Types)
         if (difficulty == "Easy")
         {
-            images = new int[10]; // Easy 난이도는 10개 이미지
+            images = new int[10];
             for (int i = 0; i < images.Length / 2; i++)
             {
-                // 인당 준비된 세 종류 카드 중 랜덤하게 하나씩 선택
                 int randomValue = UnityEngine.Random.Range(0, 3);
                 images[2 * i] = i + randomValue;
                 images[2 * i + 1] = i + randomValue;
             }
         }
+        // Normal : 20 Cards(10 Types)
         else if (difficulty == "Normal")
         {
-            images = new int[20]; // Normal 난이도는 20개 이미지
+            images = new int[20]; 
             
-            // 인당 준비된 세 종류 카드 중 앞 두개 카드 선택.(랜덤하게 선택하기엔 아이디어부족+코드 길어짐)
-            int value = 0; // 실제 images 배열에 넣을 값
+            int value = 0; 
 
             for (int i = 0; i < images.Length / 2; i++)
             {
-                // 배열에 값을 할당
                 images[2 * i] = value;
                 images[2 * i + 1] = value;
 
-                // 다음 값으로 증가시킴. 2, 5, 8, 11, 14는 건너뛰기
                 if (value == 1 || value == 4 || value == 7 || value == 10 || value == 13)
                 {
-                    value += 2; // 2, 5, 8, 11, 14를 건너뛰기 위해 2 증가
+                    value += 2;
                 }
                 else
                 {
-                    value += 1; // 그 외에는 1 증가
+                    value += 1;
                 }
             }
         }
-        else // Hard 또는 기타
+        // Hard : 30 Cards(15 Types)
+        else
         {
-            images = new int[30]; // Hard 난이도는 30개 이미지
+            images = new int[30];
             for (int i = 0; i < images.Length / 2; i++)
             {
                 images[2 * i] = i;
@@ -96,10 +83,10 @@ public class gameManager : MonoBehaviour
             }
         }
 
-        // 카드를 섞는 원래 코드
+        // shuffling cards (Before)
         //images = images.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
 
-        // 카드를 섞는 새로운 코드
+        // shuffling cards (After)
         for (int i = 0; i < images.Length; i++)
         {
             int randomIndex = UnityEngine.Random.Range(0, images.Length);
@@ -108,8 +95,7 @@ public class gameManager : MonoBehaviour
             images[randomIndex] = temp;
         }
 
-
-        // 난이도별로 다른 카드 배치
+        // Set Card Position & Get sprite : EASY
         if (difficulty == "Easy")
         {
             for (int i = 0; i < 10; i++)
@@ -134,17 +120,11 @@ public class gameManager : MonoBehaviour
                 }
                 newCard.transform.position = new Vector3(x, y, 0);
 
-                // 기존 스프라이트 이미지 리소스 이름 선정
-                //string imageName = "image" + images[i].ToString();
-
-                // 이미지 리소스 수정에 따른 스프라이트 이름 선정
-                int nameNum = images[i] / 3;
-                int subNum = images[i] % 3 + 1;
-                string imageName = ConvertIntToName(nameNum) + subNum.ToString();
-
+                string imageName = "image" + images[i].ToString();
                 newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(imageName);
             }
         }
+        // Set Card Position & Get sprite : Normal
         else if (difficulty == "Normal")
         {
             for (int i = 0; i < 20; i++)
@@ -156,18 +136,12 @@ public class gameManager : MonoBehaviour
                 float y = (i % 5) * 1.3f - 3.6f;
                 newCard.transform.position = new Vector3(x, y, 0);
 
-                // 기존 스프라이트 이미지 리소스 이름 선정
-                // string imageName = "image" + images[i].ToString();
-
-                // 이미지 리소스 수정에 따른 스프라이트 이름 선정
-                int nameNum = images[i] / 3;
-                int subNum = images[i] % 3 + 1;
-                string imageName = ConvertIntToName(nameNum) + subNum.ToString();
-
+                string imageName = "image" + images[i].ToString();
                 newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(imageName);
             }
 
         }
+        // Set Card Position & Get sprite : Hard
         else
         {
             for (int i = 0; i < 30; i++)
@@ -179,18 +153,10 @@ public class gameManager : MonoBehaviour
                 float y = (i % 6) * 1.3f - 3.6f;
                 newCard.transform.position = new Vector3(x, y, 0);
 
-                // 기존 스프라이트 이미지 리소스 이름 선정
-                // string imageName = "image" + images[i].ToString();
-
-                // 이미지 리소스 수정에 따른 스프라이트 이름 선정
-                int nameNum = images[i] / 3;
-                int subNum = images[i] % 3 + 1;
-                string imageName = ConvertIntToName(nameNum) + subNum.ToString();
-
+                string imageName = "image" + images[i].ToString();
                 newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(imageName);
             }
         }
-
     }
 
     // Update is called once per frame
@@ -233,17 +199,6 @@ public class gameManager : MonoBehaviour
 
         firstCard = null;
         secondCard = null;
-    }
-
-    // 이름 매핑 함수
-    string ConvertIntToName(int value)
-    {
-        if (nameMap.TryGetValue(value, out string name))
-        {
-            return name;
-        }
-
-        return "Unknown"; // 사전에 정의되지 않은 값의 경우
     }
 
 }
