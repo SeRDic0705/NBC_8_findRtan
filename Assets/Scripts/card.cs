6a2a02f06c6fd8477c4b6f138cf4dddf1c35665b
EcchiClone <aa01092308481@gmail.com>
2023-12-27 16:20:04 +0900

    fix: 6번 버그(카드 5초 방치 시 close 되는 기능 관련)
    - 방치된 카드가 뒤집히는 조건을 확실히 하여, 전용 함수 closeCard4sec() 새로 작성하여 사용

--- Assets/Scripts/card.cs
+++ Assets/Scripts/card.cs
@@ -13,6 +13,8 @@ public class card : MonoBehaviour
 
     private Vector3 pos;
 
+    private bool clickSecondCard; // check for close4sec
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +24,11 @@ public class card : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // check for close4sec
+        if(gameManager.I.firstCard != gameObject)
+        {
+            clickSecondCard = true;
+        }
         
     }
 
@@ -46,13 +53,15 @@ public class card : MonoBehaviour
         if (gameManager.I.firstCard == null)
         {
             gameManager.I.firstCard = gameObject;
+
             //처음 카드 선택한 시간
             flipTime = Time.time;
-            Invoke("closeCardInvoke", 4.0f);
+            Invoke("closeCard4sec", 4.0f);
         }
         else
         {
             gameManager.I.secondCard = gameObject;
+
             gameManager.I.isMatched();
         }
     }
@@ -77,6 +86,19 @@ public class card : MonoBehaviour
         transform.Find("back").GetComponent<SpriteRenderer>().color = new Color(255f / 69f, 170f / 255f, 169f / 255f);
         anim.SetBool("isOpen", false);
         transform.Find("front").gameObject.SetActive(false);
-        gameManager.I.firstCard = null;
+    }
+    void closeCard4sec()
+    {
+        if (clickSecondCard == true)
+        {
+            clickSecondCard = false;
+        }
+        else
+        {
+            transform.Find("back").GetComponent<SpriteRenderer>().color = new Color(255f / 69f, 170f / 255f, 169f / 255f);
+            anim.SetBool("isOpen", false);
+            transform.Find("front").gameObject.SetActive(false);
+            gameManager.I.firstCard = null;
+        }
     }
 }
